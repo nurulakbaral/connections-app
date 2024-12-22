e2abebb52ff60cbd246ed2b58608e7ea56ec405c
Nurul Akbar Al-Ghifari <nurulakbaral@gmail.com>
2024-12-22 11:15:02 +0700

    Fix(Layout): Build

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Box } from '~/src/ui/components'
 import { AppMain, AppMenu } from '~/src/ui/layouts'
 import { CardUserList } from '~/src/features/user/card-user-list'
 
-export function AppContent() {
+function AppContent() {
   return (
     <Box className='p-12'>
       <CardUserList />
